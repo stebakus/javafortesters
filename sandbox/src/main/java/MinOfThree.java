eb55cdf47952002e07c7514397aa7b6ea5b73cda
stebakus <36803731+stebakus@users.noreply.github.com>
2020-02-02 22:44:31 -0500

    update to min of 4

--- sandbox/src/main/java/MinOfThree.java
+++ sandbox/src/main/java/MinOfThree.java
@@ -1,16 +1,22 @@
 public class MinOfThree {
-    public static int min(int a, int b, int c) {
+    public static int min(int a, int b, int c, int d) {
         //напишите тут ваш код
-        if (Math.min (a, b) > c)
-        return c;
-        else return Math.min(a, b);
+        if (min(c, d) > min(a,b))
+                return min(a,b);
+        else return min(c,d);
 
     }
 
-    public static void main(String[] args) {
-        System.out.println(min(1, 2, 3));
-        System.out.println(min(-1, -2, -3));
-        System.out.println(min(3, 5, 3));
-        System.out.println(min(5, 5, 10));
+    public static int min(int a, int b) {
+        //напишите тут ваш код
+        return Math.min(a, b);
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(min(-20, -10));
+        System.out.println(min(-40, -10, -30, 40));
+        System.out.println(min(-20, -40, -30, 40));
+        System.out.println(min(-20, -10, -40, 40));
+        System.out.println(min(-20, -10, -30, -40));
     }
 }
