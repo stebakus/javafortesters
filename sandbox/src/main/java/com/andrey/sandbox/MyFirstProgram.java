dbb5437d5a5b6eae67eb16c0cd48bb22acb666ce
stebakus <36803731+stebakus@users.noreply.github.com>
2020-02-07 14:06:49 -0500

    added multiple methods

--- sandbox/src/main/java/com/andrey/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/com/andrey/sandbox/MyFirstProgram.java
@@ -3,6 +3,27 @@ package com.andrey.sandbox;
 public class MyFirstProgram {
 
 	public static void main (String[] args) {
-		System.out.println("Hello World!");
+		hello("world");
+		hello ("user");
+
+		double l = 5;
+		System.out.println("Ploshad kvadrata so storonoy " + l + " = " + area(l));
+
+		double a = 4;
+		double b = 6;
+
+		System.out.println("Ploshad pryamougolnika so storonami " + a + " and " + b + " = " + area(a, b));
+	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double area(double len) {
+		return len * len;
+	}
+
+	public static double area(double a, double b){
+		return a * b;
 	}
 }
