264d6b3f282b805b3983e91e99dd9a70a0988a07
stebakus <36803731+stebakus@users.noreply.github.com>
2020-02-07 13:49:41 -0500

    Update MyFirstProgram.java

--- sandbox/src/main/java/com/andrey/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/com/andrey/sandbox/MyFirstProgram.java
@@ -3,7 +3,6 @@ package com.andrey.sandbox;
 public class MyFirstProgram {
 
 	public static void main (String[] args) {
-
-	System.out.println("Hello World!");
+		System.out.println("Hello World!");
 	}
 }
