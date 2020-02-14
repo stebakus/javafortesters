9189da1e9f138e1d17eff745d0c229d07a60c7a8
stebakus <36803731+stebakus@users.noreply.github.com>
2020-02-13 23:20:28 -0500

    Removed unnesessary click from submitGroupCreation method

--- addressbook-web-tests/src/test/java/com/andrey/addressbook/ContactsCreationTests.java
+++ addressbook-web-tests/src/test/java/com/andrey/addressbook/ContactsCreationTests.java
@@ -40,7 +40,6 @@ public class ContactsCreationTests {
   }
 
   private void submitContactCreation() {
-    wd.findElement(By.name("theform")).click();
     wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
   }
 
