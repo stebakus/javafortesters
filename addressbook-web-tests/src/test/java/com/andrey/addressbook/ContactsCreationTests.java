2f40276d640401f3691d7bb759c3df2aae461417
stebakus <36803731+stebakus@users.noreply.github.com>
2020-02-13 22:57:14 -0500

    Updated Login method to match GroupCreationTests and ContactsCreationTests

--- addressbook-web-tests/src/test/java/com/andrey/addressbook/ContactsCreationTests.java
+++ addressbook-web-tests/src/test/java/com/andrey/addressbook/ContactsCreationTests.java
@@ -17,8 +17,10 @@ public class ContactsCreationTests {
   }
 
   private void login(String username, String password) {
+    wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("pass")).click();
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//input[@value='Login']")).click();
