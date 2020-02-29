1444481e31c5330ac5358b5a77ca8a5f7025717e
stebakus <36803731+stebakus@users.noreply.github.com>
2020-02-28 19:10:47 -0500

    last group deletion test

--- addressbook-web-tests/src/test/java/com/andrey/addressbook/tests/GroupModificaitonTests.java
+++ addressbook-web-tests/src/test/java/com/andrey/addressbook/tests/GroupModificaitonTests.java
@@ -15,7 +15,7 @@ public class GroupModificaitonTests extends TestBase {
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
     int before = app.getGroupHelper().getGroupCount();
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before - 1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
     app.getGroupHelper().submitGroupModification();
