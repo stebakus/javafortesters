11ccef5714f1effbf64a8f23033c032bc4fb1d4b
stebakus <36803731+stebakus@users.noreply.github.com>
2020-03-13 11:56:01 -0400

    adding log lec 6.11

--- addressbook-web-tests/src/test/java/com/andrey/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/com/andrey/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,8 @@ import com.andrey.addressbook.models.Groups;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.*;
 
 import java.io.*;
