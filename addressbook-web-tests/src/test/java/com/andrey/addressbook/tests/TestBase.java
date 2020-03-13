30804656c99237d6f4a3e5c5d94483d0761bf4b7
stebakus <36803731+stebakus@users.noreply.github.com>
2020-03-13 11:08:18 -0400

    ZADANIE 14

--- addressbook-web-tests/src/test/java/com/andrey/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/com/andrey/addressbook/tests/TestBase.java
@@ -5,17 +5,20 @@ import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import java.io.IOException;
+
 public class TestBase {
 
-  public static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  public static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
 
   @BeforeSuite
-  public void setUp() throws Exception {
+  public void setUp() throws IOException {
     app.init();
   }
 
   @AfterSuite
-  public void tearDown() throws Exception {
+  public void tearDown() {
     app.stop();
   }
 
