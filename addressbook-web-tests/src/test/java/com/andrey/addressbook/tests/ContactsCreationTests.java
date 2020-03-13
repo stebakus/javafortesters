15a50775506767fa4ee215df3cec8830d74e21c1
stebakus <36803731+stebakus@users.noreply.github.com>
2020-03-12 21:25:08 -0400

    lec 6.5 contact creation + contact generator

--- addressbook-web-tests/src/test/java/com/andrey/addressbook/tests/ContactsCreationTests.java
+++ addressbook-web-tests/src/test/java/com/andrey/addressbook/tests/ContactsCreationTests.java
@@ -5,7 +5,7 @@ import com.andrey.addressbook.models.ContactsData;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -16,21 +16,16 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactsCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validContacts() {
+  public Iterator<Object[]> validContacts() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    File photo = new File("src/test/resources/my_photo.jpg");
-    list.add(new Object[]{new ContactsData()
-            .withFirstname("firstname 1").withLastname("lastname 1")
-            .withAddress("address 1").withMobilePhone("1 214192300")
-            .withEmailaddress("andreybegishev1@gmail.com").withGroup("[none]").withPhoto(photo)});
-    list.add(new Object[]{new ContactsData()
-            .withFirstname("firstname 2").withLastname("lastname 2")
-            .withAddress("address 2").withMobilePhone("2 214192300")
-            .withEmailaddress("andreybegishev2@gmail.com").withGroup("[none]").withPhoto(photo)});
-    list.add(new Object[]{new ContactsData()
-            .withFirstname("firstname 3").withLastname("lastname 3")
-            .withAddress("address 3").withMobilePhone("3 214192300")
-            .withEmailaddress("andreybegishev3@gmail.com").withGroup("[none]").withPhoto(photo)});
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[] {new ContactsData().withFirstname(split[0]).withLastname(split[1]).withAddress(split[2])
+              .withMobilePhone(split[3]).withEmailaddress(split[4]).withGroup(split[5]).withPhoto(new File((split[6])))});
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
