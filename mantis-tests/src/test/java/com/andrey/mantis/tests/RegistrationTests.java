189d79e680c8e1d3a6af4d23e614a6dd7c644430
stebakus <36803731+stebakus@users.noreply.github.com>
2020-03-27 17:26:05 -0400

    fix captcha

--- mantis-tests/src/test/java/com/andrey/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/com/andrey/mantis/tests/RegistrationTests.java
@@ -1,12 +1,48 @@
 package com.andrey.mantis.tests;
 
+import com.andrey.mantis.models.MailMessage;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.lanwen.verbalregex.VerbalExpression;
 
-public class RegistrationTests extends TestBase{
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.List;
 
+public class RegistrationTests extends TestBase {
   @Test
-  public void testRegistration () {
+  public void testRegistration() throws IOException, MessagingException {
     app.registration().start("user1", "user1@localhost.localdomain");
-
   }
 }
+  /*@BeforeMethod
+  public void startMailServer() {
+    app.mail().start();
+  }
+
+  @Test
+  public void testRegistration () throws IOException, MessagingException {
+    String user = "user1";
+    String email = "user1@localhost.localdomain";
+    app.registration().start(user, email);
+    List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    String confirmationLink = findConfirmationLink(mailMessages, email);
+    app.registration().finish(confirmationLink, "password");
+    app.newSession().login(user);
+  }
+
+  private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
+    MailMessage mailMessage = mailMessages.stream().filter((m) -> m.to.equals(email)).findFirst().get();
+    VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
+    return regex.getText(mailMessage.text);
+  }
+
+
+  @AfterMethod (alwaysRun = true)
+  public void stopMailServer() {
+    app.mail().stop();
+  }
+}
+
+   */
