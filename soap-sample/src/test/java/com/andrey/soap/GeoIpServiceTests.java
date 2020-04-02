6b4cd920da374d6cb340be7da49e957f28a4f62f
stebakus <36803731+stebakus@users.noreply.github.com>
2020-04-02 19:26:48 -0400

    lec 9.2 Page Not Found

---
+++ soap-sample/src/test/java/com/andrey/soap/GeoIpServiceTests.java
@@ -0,0 +1,16 @@
+package com.andrey.soap;
+
+import net.webservicex.GeoIP;
+import net.webservicex.GeoIPService;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class GeoIpServiceTests {
+
+  @Test
+  public void testMyIp() {
+    GeoIP geoIP = new GeoIPService().getGeoIPServiceSoap12().getGeoIP("97.103.181.121");
+    assertEquals(geoIP.getCountryCode(), "US");
+  }
+}
