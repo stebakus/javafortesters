33373adb1f6ae1180409c9ba14d9701fd40f26e6
stebakus <36803731+stebakus@users.noreply.github.com>
2020-04-03 15:41:32 -0400

    podgotovka k zadaniyu 20

--- rest-sample/src/test/java/com/andrey/rest/Issue.java
+++ rest-sample/src/test/java/com/andrey/rest/Issue.java
@@ -7,6 +7,16 @@ public class Issue {
   private int id;
   private String subject;
   private String description;
+  private String state_name;
+
+  public String getStatus() {
+    return state_name;
+  }
+
+  public Issue withStatus(String state_name) {
+    this.state_name = state_name;
+    return this;
+  }
 
   public int getId() {
     return id;
@@ -22,6 +32,32 @@ public class Issue {
     return description;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Issue issue = (Issue) o;
+    return id == issue.id &&
+            Objects.equals(subject, issue.subject) &&
+            Objects.equals(description, issue.description) &&
+            Objects.equals(state_name, issue.state_name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, subject, description, state_name);
+  }
+
+  @Override
+  public String toString() {
+    return "Issue{" +
+            "id=" + id +
+            ", subject='" + subject + '\'' +
+            ", description='" + description + '\'' +
+            ", status='" + state_name + '\'' +
+            '}';
+  }
+
   public Issue withId(int id) {
     this.id = id;
     return this;
@@ -30,21 +66,6 @@ public class Issue {
   public Issue withSubject(String subject) {
     this.subject = subject;
     return this;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    Issue issue = (Issue) o;
-    return id == issue.id &&
-            Objects.equals(subject, issue.subject) &&
-            Objects.equals(description, issue.description);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, subject, description);
   }
 
   public Issue withDescription(String description) {
---
+++ rest-sample/src/test/java/com/andrey/rest/TestBase.java
@@ -0,0 +1,89 @@
+package com.andrey.rest;
+
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.message.BasicNameValuePair;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.SkipException;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import java.io.IOException;
+import java.util.Set;
+
+
+public class TestBase {
+
+  public static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+
+  Set<Issue> getIssues() throws IOException {
+    String json = getExecutor().execute(Request.Get("https://bugify.stqa.ru/api/issues.json?limit=500"))
+            .returnContent().asString();
+    JsonElement parsed = new JsonParser().parse(json);
+    JsonElement issues = parsed.getAsJsonObject().get("issues");
+    return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
+  }
+
+  private Executor getExecutor() {
+    return Executor.newInstance().auth("288f44776e7bec4bf44fdfeb1e646490", "");
+  }
+
+  int createIssue(Issue newIssue) throws IOException {
+    String json = getExecutor().execute(Request.Post("https://bugify.stqa.ru/api/issues.json")
+            .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject()),
+                    new BasicNameValuePair("description", newIssue.getDescription())))
+            .returnContent().asString();
+    JsonElement parsed = new JsonParser().parse(json);
+    return parsed.getAsJsonObject().get("issue_id").getAsInt();
+  }
+
+
+  boolean isIssueOpen(int issueId) throws IOException {
+    Issue issue = getIssues().iterator().next();
+    if (issue.getStatus().equals("Closed") || issue.getStatus().equals("Resolved") || issue.getStatus().equals("Deleted")) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  public void skipIfNotFixed(int issueId) throws IOException {
+    if (isIssueOpen(issueId)) {
+      throw new SkipException("Ignored because of issue " + issueId);
+    }
+  }
+
+
+  @BeforeSuite
+  public void setUp() throws IOException {
+    app.init();
+  }
+
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() throws IOException {
+    app.stop();
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
